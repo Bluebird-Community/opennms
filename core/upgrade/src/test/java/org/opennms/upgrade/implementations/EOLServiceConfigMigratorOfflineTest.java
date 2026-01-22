d14d7efa84cad42a785699552ad5894733af1b37
Ronny Trommer <ronny@no42.org>
2026-01-22 10:11:36 +0100

    chore: Fix EOL service test
    Signed-off-by: Ronny Trommer <ronny@no42.org>

--- core/upgrade/src/test/java/org/opennms/upgrade/implementations/EOLServiceConfigMigratorOfflineTest.java
+++ core/upgrade/src/test/java/org/opennms/upgrade/implementations/EOLServiceConfigMigratorOfflineTest.java
@@ -60,9 +60,7 @@ public class EOLServiceConfigMigratorOfflineTest {
             "OpenNMS:Name=Xmlrpcd",
             "OpenNMS:Name=XmlrpcProvisioner",
             "OpenNMS:Name=AccessPointMonitor",
-            "OpenNMS:Name=PollerBackEnd",
-            "OpenNMS:Name=Ackd",
-            "OpenNMS:Name=Tl1d"
+            "OpenNMS:Name=PollerBackEnd"
     );
 
     public EOLServiceConfigMigratorOfflineTest(final String testFile, final int totalBefore, final int totalAfter, final int enabledAfter) {
@@ -98,13 +96,12 @@ public class EOLServiceConfigMigratorOfflineTest {
     public static Collection<Object[]> params() {
         return Arrays.asList(new Object[][] {
             // service config, total, enabled
-            { "target/home/etc/service-configuration-1.8.17.xml",  38, 38, 34 },
-            { "target/home/etc/service-configuration-1.10.14.xml", 38, 38, 34 },
-            { "target/home/etc/service-configuration-1.12.9.xml",  39, 39, 34 },
-            { "target/home/etc/service-configuration-14.0.3.xml",  38, 38, 26 },
-            { "target/home/etc/service-configuration-15.0.2.xml",  38, 38, 26 },
-            { "target/home/etc/service-configuration-16.0.4.xml",  37, 37, 26 },
-            { "target/home/etc/service-configuration-33.0.5.xml",  35, 35, 24 }
+            { "target/home/etc/service-configuration-1.8.17.xml",  38, 38, 32 },
+            { "target/home/etc/service-configuration-1.10.14.xml", 38, 38, 32 },
+            { "target/home/etc/service-configuration-1.12.9.xml",  39, 39, 32 },
+            { "target/home/etc/service-configuration-14.0.3.xml",  38, 38, 25 },
+            { "target/home/etc/service-configuration-15.0.2.xml",  38, 38, 25 },
+            { "target/home/etc/service-configuration-16.0.4.xml",  37, 37, 25 },
         });
     }
 
